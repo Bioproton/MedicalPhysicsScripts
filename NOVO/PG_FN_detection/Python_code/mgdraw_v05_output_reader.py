66fc4a872128a8d5da1c7523c2cfef6ad681d085
SanderThu <sander.thu@uib.no>
2025-08-07 15:58:43 +0200

    Add files via upload

--- NOVO/PG_FN_detection/Python_code/mgdraw_v05_output_reader.py
+++ NOVO/PG_FN_detection/Python_code/mgdraw_v05_output_reader.py
@@ -26,6 +26,7 @@ def collect_txt_data(file_adress, primaries_per_spawn=2e6) -> str:
     icode = [] # Interaction codes (ICODES): What kind of interaction happened?  
     particle_in = []   # JTRACK values: What particle initiated the interaction?
     particle_out = []  # KPART(IP) values: What secondary resulted from the interaction?
+    fnpg_flag = [] # LLOUSE values: Does the interaction stem directly from target FN/PG-production?
 
     targetZ = []   # ICHTAR values: What is the Z-value of the target particle?
     targetA = []   # IBTAR values: What is the A-value of the target particle?
@@ -59,26 +60,27 @@ def collect_txt_data(file_adress, primaries_per_spawn=2e6) -> str:
             icode.append(int(line.split(" ")[1]))
             particle_in.append(int(line.split(" ")[2]))
             particle_out.append(int(line.split(" ")[3]))
+            fnpg_flag.append(int(line.split(" ")[4]))
 
-            targetZ.append(int(line.split(" ")[4]))
-            targetA.append(int(line.split(" ")[5]))
-            energy_out.append(float(line.split(" ")[6]))
-            energy_in.append(float(line.split(" ")[7]))
+            targetZ.append(int(line.split(" ")[5]))
+            targetA.append(int(line.split(" ")[6]))
+            energy_out.append(float(line.split(" ")[7]))
+            energy_in.append(float(line.split(" ")[8]))
 
-            crash_x.append(float(line.split(" ")[8]))
-            crash_y.append(float(line.split(" ")[9]))
-            crash_z.append(float(line.split(" ")[10]))
+            crash_x.append(float(line.split(" ")[9]))
+            crash_y.append(float(line.split(" ")[10]))
+            crash_z.append(float(line.split(" ")[11]))
 
-            region.append(int(line.split(" ")[11]))
-            particle_generation.append(int(line.split(" ")[12]))
-            particle_age.append(float(line.split(" ")[13]))
+            region.append(int(line.split(" ")[12]))
+            particle_generation.append(int(line.split(" ")[13]))
+            particle_age.append(float(line.split(" ")[14]))
 
-            source_x.append(float(line.split(" ")[14]))
-            source_y.append(float(line.split(" ")[15]))
-            source_z.append(float(line.split(" ")[16]))                         
+            source_x.append(float(line.split(" ")[15]))
+            source_y.append(float(line.split(" ")[16]))
+            source_z.append(float(line.split(" ")[17]))                         
 
     print(f"\rCollecting data: complete. {n} lines read. Time used: {round(time.time() - time_stamp, 3)} s", flush=True)
-    return ncase, icode, particle_in, particle_out, \
+    return ncase, icode, particle_in, particle_out, fnpg_flag, \
         targetZ, targetA, energy_out, energy_in, \
         crash_x, crash_y, crash_z, \
         region, particle_generation, particle_age, \
